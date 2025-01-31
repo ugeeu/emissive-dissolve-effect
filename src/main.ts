3b87b20b2c7ac7a14c32afb58ed7b26f4ca9df2a
Jatin Chopra <jatinchopra2053@gmail.com>
2025-02-01 03:02:55 +0530

    08 - updated particle system with distance-scaling , rotation & texture

--- src/main.ts
+++ src/main.ts
@@ -67,6 +67,10 @@ async function loadTextures() {
 loadTextures();
 
 
+let particleTexture: THREE.Texture;
+particleTexture = new THREE.TextureLoader().load('/particle.png')
+
+
 let mesh: THREE.Object3D;
 let meshGeo: THREE.BufferGeometry;
 
@@ -160,11 +164,15 @@ scene.add(mesh);
 
 let particleMesh: THREE.Points;
 let particleMat = new THREE.ShaderMaterial();
+particleMat.transparent = true;
+particleMat.blending = THREE.AdditiveBlending;
 let particleCount = meshGeo.attributes.position.count;
 let particleMaxOffsetArr: Float32Array; // -- how far a particle can go from its initial position 
 let particleInitPosArr: Float32Array; // store the initial position of the particles -- particle position will reset here if it exceed maxoffset
 let particleCurrPosArr: Float32Array; // use to update he position of the particle 
 let particleVelocityArr: Float32Array; // velocity of each particle
+let particleDistArr: Float32Array;
+let particleRotationArr: Float32Array;
 let particleData = {
     particleSpeedFactor: 0.02, // for tweaking velocity 
     velocityFactor: { x: 1, y: 1 },
@@ -177,6 +185,9 @@ function initParticleAttributes() {
     particleInitPosArr = new Float32Array(meshGeo.getAttribute('position').array);
     particleCurrPosArr = new Float32Array(meshGeo.getAttribute('position').array);
     particleVelocityArr = new Float32Array(particleCount * 3);
+    particleDistArr = new Float32Array(particleCount);
+    particleRotationArr = new Float32Array(particleCount);
+
 
     for (let i = 0; i < particleCount; i++) {
         let x = i * 3 + 0;
@@ -188,11 +199,17 @@ function initParticleAttributes() {
         particleVelocityArr[x] = Math.random() * 0.5 + 0.5;
         particleVelocityArr[y] = Math.random() * 0.5 + 0.5;
         particleVelocityArr[z] = Math.random() * 0.1;
+
+        particleDistArr[i] = 0.001;
+        particleRotationArr[i] = Math.random() * Math.PI * 2;
+
     }
 
     meshGeo.setAttribute('aOffset', new THREE.BufferAttribute(particleMaxOffsetArr, 1));
     meshGeo.setAttribute('aCurrentPos', new THREE.BufferAttribute(particleCurrPosArr, 3));
     meshGeo.setAttribute('aVelocity', new THREE.BufferAttribute(particleVelocityArr, 3));
+    meshGeo.setAttribute('aDist', new THREE.BufferAttribute(particleDistArr, 1));
+    meshGeo.setAttribute('aAngle', new THREE.BufferAttribute(particleRotationArr, 1));
 }
 
 
@@ -261,6 +278,9 @@ function updateParticleAttriutes() {
         const vec2 = new THREE.Vector3(particleCurrPosArr[x], particleCurrPosArr[y], particleCurrPosArr[z]);
         const dist = vec1.distanceTo(vec2);
 
+        particleDistArr[i] = dist;
+        particleRotationArr[i] += 0.01;
+
         if (dist > particleMaxOffsetArr[i]) {
             particleCurrPosArr[x] = particleInitPosArr[x];
             particleCurrPosArr[y] = particleInitPosArr[y];
@@ -271,6 +291,8 @@ function updateParticleAttriutes() {
     meshGeo.setAttribute('aOffset', new THREE.BufferAttribute(particleMaxOffsetArr, 1));
     meshGeo.setAttribute('aCurrentPos', new THREE.BufferAttribute(particleCurrPosArr, 3));
     meshGeo.setAttribute('aVelocity', new THREE.BufferAttribute(particleVelocityArr, 3));
+    meshGeo.setAttribute('aDist', new THREE.BufferAttribute(particleDistArr, 1));
+    meshGeo.setAttribute('aAngle', new THREE.BufferAttribute(particleRotationArr, 1));
 }
 
 
@@ -278,6 +300,9 @@ initParticleAttributes();
 
 
 const particlesUniformData = {
+    uTexture: {
+        value: particleTexture,
+    },
     uPixelDensity: {
         value: re.getPixelRatio()
     },
@@ -307,14 +332,19 @@ uniform float uEdge;
 uniform float uProgress;
 
 varying float vNoise;
+varying float vAngle;
 
 attribute vec3 aCurrentPos;
+attribute float aDist;
+attribute float aAngle;
 
 void main() {
     vec3 pos = position;
 
     float noise = cnoise(pos * uFreq) * uAmp;
     vNoise =noise;
+
+    vAngle = aAngle;
 
     if( vNoise > uProgress && vNoise < uProgress + uEdge){
         pos = aCurrentPos;
@@ -325,7 +355,9 @@ void main() {
     vec4 projectedPosition = projectionMatrix * viewPosition;
     gl_Position = projectedPosition;
 
-    gl_PointSize = (uBaseSize * uPixelDensity) / -viewPosition.z;
+    float size = uBaseSize * uPixelDensity;
+    size = size  / (aDist + 1.0);
+    gl_PointSize = size / -viewPosition.z;
 }
 `;
 
@@ -333,14 +365,26 @@ particleMat.fragmentShader = `
 uniform vec3 uColor;
 uniform float uEdge;
 uniform float uProgress;
+uniform sampler2D uTexture;
 
 varying float vNoise;
+varying float vAngle;
 
 void main(){
     if( vNoise < uProgress ) discard;
     if( vNoise > uProgress + uEdge) discard;
 
+    vec2 coord = gl_PointCoord;
+    coord = coord - 0.5; // get the coordinate from 0-1 ot -0.5 to 0.5
+    coord = coord * mat2(cos(vAngle),sin(vAngle) , -sin(vAngle), cos(vAngle)); // apply the rotation transformaion
+    coord = coord +  0.5; // reset the coordinate to 0-1  
+
+
+    vec4 texture = texture2D(uTexture,coord);
+
+
     gl_FragColor = vec4(uColor,1.0);
+    gl_FragColor = vec4(vec3(uColor.xyz * texture.xyz),1.0);
 }
 `;
 
